f6c14fe7fb2c590b546f502b0ce99c1e61f48c51
Leonel Aguilera <laac2808@gmail.com>
2022-11-13 19:02:48 +0100

    Creado codificador binario

---
+++ ClaveSecreta/ClaveSecreta.srcs/sources_1/new/codificador.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 13.11.2022 14:44:28
+-- Design Name: 
+-- Module Name: codificador - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.math_real.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity codificador is
+    GENERIC(
+        N: POSITIVE:=5
+    );
+    PORT(
+        DIN : IN STD_LOGIC_VECTOR (N-1 downto 0);
+        CLK: IN STD_LOGIC;
+        COUT : OUT STD_LOGIC_VECTOR(INTEGER(CEIL(LOG2(REAL(N))))-1 downto 0)
+    );
+end codificador;
+
+architecture Behavioral of codificador is
+begin
+    process(CLK)
+    VARIABLE COUT_temp: UNSIGNED(INTEGER(CEIL(LOG2(REAL(N))))-1 DOWNTO 0);
+    begin
+        IF CLK = '1' THEN
+            FOR i IN 0 TO N-1 LOOP
+                COUT_temp := to_unsigned(i,INTEGER(CEIL(LOG2(REAL(N)))));
+            END LOOP;
+        END IF;
+        COUT <= STD_LOGIC_VECTOR(COUT_temp);         
+    end process;
+end Behavioral;
