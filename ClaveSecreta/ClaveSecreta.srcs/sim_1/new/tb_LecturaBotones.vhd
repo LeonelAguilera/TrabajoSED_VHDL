9ecfdd5d552befdc0f365167c088ea17fe3fae2b
tavo7 <tavotito7@gmail.com>
2022-12-05 00:23:40 +0100

    tb_lecturabotones

---
+++ ClaveSecreta/ClaveSecreta.srcs/sim_1/new/tb_LecturaBotones.vhd
@@ -0,0 +1,59 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+package button_pkg is
+    type button_array is array(natural range <>) of std_logic_vector;
+end package;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.math_real.all;
+use work.button_pkg.all;
+
+entity tb_LecturaBotones is
+    GENERIC(
+        N_master: POSITIVE:=5;
+        M_master: POSITIVE:=10
+    );
+end tb_LecturaBotones;
+
+architecture tb of tb_LecturaBotones is
+
+    component LecturaBotones
+        port (S_IN  : in std_logic_vector (n_master-1 downto 0);
+              clr   : in std_logic;
+              S_OUT : out button_array (m_master-1 downto 0)(integer(ceil(log2(real(n_master))))-1 downto 0));
+    end component;
+
+    signal S_IN  : std_logic_vector (n_master-1 downto 0);
+    signal clr   : std_logic;
+    signal S_OUT : button_array (m_master-1 downto 0)(integer(ceil(log2(real(n_master))))-1 downto 0);
+
+    constant TbPeriod : time := 100 ns; 
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : LecturaBotones
+    port map (S_IN  => S_IN,
+              clr   => clr,
+              S_OUT => S_OUT);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    stimuli : process
+    begin
+        S_IN <= (others => '0');
+        clr <= '1';
+        wait for 100 ns;
+        clr <= '0';
+        wait for 100 ns;
+
+        wait for 100 * TbPeriod;
+
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
